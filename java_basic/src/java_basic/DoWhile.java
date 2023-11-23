5e263b4d411831635b1ae7035fef3bcccee6bd95
micosara365@gmail.com <micosara365@gmail.com>
2023-11-23 17:56:56 +0900

    11/23 수업자료 : while/array

---
+++ java_basic/src/java_basic/DoWhile.java
@@ -0,0 +1,16 @@
+package java_basic;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		int temp=10;
+		
+		do{
+			System.out.println(temp);
+			temp++;
+		}while(temp < 10);
+
+	}
+
+}
--- java_basic/src/java_basic/Kawibawibo.java
+++ java_basic/src/java_basic/Kawibawibo.java
@@ -14,53 +14,70 @@ public class Kawibawibo {
 		int menu = 0;
 		String juge = "";
 
-		// 입력
-		com_kwb_num = (int) (Math.random() * 3);
+		String quit = "";
 
-		System.out.println("컴퓨터는 결정했습니다.");
-		System.out.println("당신의 선택은?");
-		System.out.println("1.가위 2.바위 3.보");
+		while (!quit.toUpperCase().equals("Q")) {
+			// 입력
+			com_kwb_num = (int) (Math.random() * 3);
 
-		Scanner scann = new Scanner(System.in);
-		menu = scann.nextInt();
-		my_kwb_num = menu - 1;
+			System.out.println("컴퓨터는 결정했습니다.");
+			System.out.println("당신의 선택은?");
 
-		if (com_kwb_num == ((my_kwb_num + 1) % 3)) { // 졌어요...
-			juge = "졌습니다.";
-		} else {
-			if (com_kwb_num == my_kwb_num) { // 비겼어요..
-				juge = "비겼어요.";
-			} else { // 이겼어요.
-				juge = "이겼어요.";
+			Scanner scann = new Scanner(System.in);
+			while(true) {
+				System.out.println("1.가위 2.바위 3.보");
+				menu = scann.nextInt();scann.nextLine();
+				
+				if (menu < 1 || menu > 3) {
+					System.out.println("입력이 올바르지 않습니다.");
+				}else {
+					break;
+				}
 			}
-		}
-		
-		switch (my_kwb_num) {
-		case 0:
-			my_kwb = "가위";
-			break;
-		case 1:
-			my_kwb = "바위";
-			break;
-		case 2:
-			my_kwb = "보";
-		}
+			
+			my_kwb_num = menu - 1;
 
-		switch (com_kwb_num) {
-		case 0:
-			com_kwb = "가위";
-			break;
-		case 1:
-			com_kwb = "바위";
-			break;
-		case 2:
-			com_kwb = "보";
-		}
+			if (com_kwb_num == ((my_kwb_num + 1) % 3)) { // 졌어요...
+				juge = "졌습니다.";
+			} else {
+				if (com_kwb_num == my_kwb_num) { // 비겼어요..
+					juge = "비겼어요.";
+				} else { // 이겼어요.
+					juge = "이겼어요.";
+				}
+			}
 
-		// 출력
-		System.out.println("컴퓨터 : " + com_kwb);
-		System.out.println("당신 : " + my_kwb);
-		System.out.println(juge);
+			switch (my_kwb_num) {
+			case 0:
+				my_kwb = "가위";
+				break;
+			case 1:
+				my_kwb = "바위";
+				break;
+			case 2:
+				my_kwb = "보";
+			}
+
+			switch (com_kwb_num) {
+			case 0:
+				com_kwb = "가위";
+				break;
+			case 1:
+				com_kwb = "바위";
+				break;
+			case 2:
+				com_kwb = "보";
+			}
+
+			// 출력
+			System.out.println("컴퓨터 : " + com_kwb);
+			System.out.println("당신 : " + my_kwb);
+			System.out.println(juge);
+
+			System.out.println("더 할거에요?(그만:q)");
+			System.out.println("계속하려면 아무키나 누르세요");
+			quit = scann.nextLine();
+		}
 
 	}
 
