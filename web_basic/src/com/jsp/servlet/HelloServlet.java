d26b3865f18b107446d6a2d22416a4fede7ee061
micosara365@gmail.com <micosara365@gmail.com>
2024-01-02 15:59:30 +0900

    12/19 수업내용

--- web_basic/src/com/jsp/servlet/HelloServlet.java
+++ web_basic/src/com/jsp/servlet/HelloServlet.java
@@ -9,11 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello.html")
+//@WebServlet("/hello.html")
 public class HelloServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		request.setAttribute("method", "GET");
+		request.getRequestDispatcher("/WEB-INF/views/hello.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("method", "POST");
+		request.getRequestDispatcher("/WEB-INF/views/hello.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String message = request.getParameter("message");
 		String who = request.getParameter("who");
 		
@@ -25,13 +36,20 @@ public class HelloServlet extends HttpServlet {
 		out.println("<title>Echo message</title>");
 		out.println("</head>");
 		out.println("<body>");
+		out.println("<h1>method : DELETE</h1>");
 		out.println("<h1> To : "+who+"</h1>");
 		out.println("<h1> Message : "+message+"</h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
+	
+	
 
 }
+
+
+
+
+
+
+
