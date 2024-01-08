346e67079c53e11d845701f39e9e002fac8cbc57
micosara365@gmail.com <micosara365@gmail.com>
2024-01-08 18:09:58 +0900

    01/08 수업내용

--- spring_mybatis/src/main/java/com/spring/controller/MemberController.java
+++ spring_mybatis/src/main/java/com/spring/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.spring.controller;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.spring.command.MemberRegistCommand;
 import com.spring.command.PageMaker;
 import com.spring.dto.MemberVO;
 import com.spring.service.SearchMemberService;
@@ -26,6 +31,44 @@ public class MemberController {
 		
 		model.addAttribute("memberList", memberList);
 	}
+	
+	@GetMapping("/registForm")
+	public String registForm() {
+		String url="/member/regist";
+		return url;
+	}
+	
+	@GetMapping("idCheck")
+	@ResponseBody
+	public String idCheck(String id)throws Exception{
+		String message = "duplicated";
+		
+		MemberVO member = memberService.detail(id);
+		
+		if(member==null) {
+			message="";
+		}
+		
+		return message;
+	}
+	
+	@PostMapping(value="/regist", produces = "text/plain;charset=utf-8")
+	public String regist(MemberRegistCommand regCommand)throws Exception{
+		String url="/member/regist_success";
+	
+		MultipartFile multi = regCommand.getPicture();
+		
+		System.out.println(multi.getOriginalFilename());
+		File picture = new File("c:\\member\\picture",multi.getOriginalFilename());
+		picture.mkdirs();
+		
+		multi.transferTo(picture);
+		
+		
+		return url;
+	}
+	
+	
 }
 
 
